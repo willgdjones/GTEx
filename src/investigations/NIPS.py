4548bf062ec4c02da73410cb747591049146c5b6
William Jones <williamgdjones@gmail.com>
2017-10-02 15:40:18 +0100

    Question 3A

--- src/investigations/NIPS.py
+++ src/investigations/NIPS.py
@@ -12,10 +12,18 @@ sys.path.insert(0, os.getcwd())
 from src.utils.helpers import *
 import pyensembl
 os.environ['PYENSEMBL_CACHE_DIR'] = '/hps/nobackup/research/stegle/users/willj/GTEx'
+import statsmodels.stats.multitest as smm
+from pyensembl import EnsemblRelease
+from gprofiler import GProfiler
+data = EnsemblRelease(77)
+import multiprocess as mp
+from tqdm import tqdm
 
 
 
 GTEx_directory = '/hps/nobackup/research/stegle/users/willj/GTEx'
+
+os.environ['PYENSEMBL_CACHE_DIR'] = GTEx_directory
 
 parser = argparse.ArgumentParser(description='Collection of experiments. Runs on the cluster.')
 parser.add_argument('-g', '--group', help='Experiment group', required=True)
@@ -23,6 +31,17 @@ parser.add_argument('-n', '--name', help='Experiment name', required=True)
 args = vars(parser.parse_args())
 group = args['group']
 name = args['name']
+
+
+def get_gene_name(transcript):
+    transcript_id = transcript.decode('utf-8').split('.')[0]
+    return data.gene_name_of_gene_id(transcript_id)
+
+def lookup_enrichment(gene_set):
+    clean_gene_set = [x for x in gene_set if x is not None]
+    gp = GProfiler("GTEx/wj")
+    enrichment_results = gp.gprofile(clean_gene_set)
+    return enrichment_results
 
 
 class NIPSQuestion1():
@@ -111,7 +130,7 @@ class NIPSQuestion2():
                         tf_idx = [list(ths).index(x) for x in TFs]
 
                         # Take log of SMTSISH
-                        tfs[TFs.index('SMTSISCH')] = np.log2(tfs[TFs.index('SMTSISCH')] + 1)
+                        tfs[tfs.index('SMTSISCH')] = np.log2(tfs[tfs.index('SMTSISCH')] + 1)
                         tf_predictors = tfs[:,tf_idx]
                         # tf_predictors = np.array([t[np.random.permutation(tf_predictors_raw.shape[0])] for t in tf_predictors.T]).T
 
@@ -165,15 +184,64 @@ class NIPSQuestion2():
 
 
 
-class NIPSQuestion3():
+class NIPSQuestion3A():
 
 
     @staticmethod
     def gene_expression_variability():
 
-        association_results, most_varying_feature_indexes, filt_transcriptIDs = pickle.load(open(GTEx_directory + '/intermediate_results/PCCorrectedFeatureAssociations/corrected_pvalues.pickle', 'rb'))
+        os.makedirs(GTEx_directory + '/results/{}'.format(group), exist_ok=True)
+        final_results = {}
+        
+        Y, X, dIDs, tIDs, tfs, ths, t_idx = extract_final_layer_data('Lung', 'retrained', 'mean', '256')
+        tfs[list(ths).index('SMTSISCH')] = np.log2(tfs[list(ths).index('SMTSISCH')] + 1)
 
-        import pdb; pdb.set_trace()
+        tf_X = X[t_idx,:]
+
+        def pbar_update(x):
+            pbar.update(1)
+
+        def calculate_tf_enrichment(th):
+
+            i = list(ths).index(th)
+            tf = tfs[:, i].reshape(-1,1)
+
+
+            Rs = []
+            pvs = []
+            for t in range(tf_X.shape[1]):
+                R, pv = pearsonr(tf.flatten(), tf_X[:,t])
+                Rs.append(R)
+                pvs.append(pv)
+
+
+            significant_transcripts = tIDs[smm.multipletests(pvs, method='bonferroni',alpha=0.01)[0]]
+
+            significant_genes = []
+            for t in significant_transcripts:
+                try:
+                    g = get_gene_name(t)
+                except ValueError:
+                    g = None
+                significant_genes.append(g)
+
+            enrichments = lookup_enrichment(significant_genes)
+            return enrichments
+
+
+        pool = mp.Pool(processes=16)
+        pbar = tqdm(total=len(list(ths)))
+
+
+
+        results = [pool.apply_async(calculate_tf_enrichment, args=(th,), callback=pbar_update) for th in list(ths)]
+        enrichment_results = [p.get() for p in results]
+
+        final_results[key] = enrichment_results
+
+        pickle.dump(final_results, open(GTEx_directory + '/results/{group}/{name}.pickle'.format(group=group, name=name), 'wb'))
+
+
 
 
 
